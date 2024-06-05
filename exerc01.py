fc76dcc6d6cfa192af07d4b57a51783933a08009
Sharkzn <richardpereiraviera@gmail.com>
2024-06-05 20:05:09 -0300

    aula logica 05-06

---
+++ exerc01.py
@@ -0,0 +1,14 @@
+nota1 = float(input("digite sua nota de português:"))
+nota2 = float(input("digite sua nota de matematica:"))
+nota3 = float(input("digite sua nota de logistica:"))
+
+média = (nota1 + nota2 + nota3) / 3
+
+#verificar a média
+if média >= 7:
+    print("O aluno foi APROVADO!!!!")
+elif média <= 5:
+    print("O aluno esta em RECUPERAÇÂO!!!!")
+else:
+    print("O aluno foi REPROVADO!!!!")
+    
